a744c702bae2f2a6eeefc6b9cfda03774ea10621
Emil Bay <github@tixz.dk>
2020-06-16 12:51:38 +0200

    Add tests

--- test.js
+++ test.js
@@ -1,16 +1,56 @@
+const test = require('tape')
 const InvertedPromise = require('.')
 
-var iP = new InvertedPromise()
+test('resolve', function (assert) {
+  assert.plan(1)
+  const p = new InvertedPromise()
 
-var iP2 = new InvertedPromise()
+  p.then(value => {
+    assert.ok(value === 'ok')
+  }).catch(err => {
+    assert.error(err)
+  })
 
-iP.then((value) => {
-  console.log(value)
+  p.resolve('ok')
 })
 
-iP2.catch((err) => {
-  console.log(err)
+test('resolve', function (assert) {
+  assert.plan(1)
+  const p = new InvertedPromise()
+
+  p.then(value => {
+    assert.error(new Error())
+  }).catch(err => {
+    assert.ok(err === 'ok')
+  })
+
+  p.reject('ok')
 })
 
-iP.resolve('Hello')
-iP2.reject('World')
+test('resolve twice', function (assert) {
+  assert.plan(1)
+  const p = new InvertedPromise()
+
+  p.then(value => {
+    assert.ok(value === 'ok')
+  }).catch(err => {
+    assert.error(err)
+  })
+
+  p.resolve('ok')
+  p.resolve('ok2')
+})
+
+test('resolve then reject', function (assert) {
+  assert.plan(1)
+  const p = new InvertedPromise()
+
+  p.then(value => {
+    assert.ok(value === 'ok')
+  }).catch(err => {
+    assert.error(err)
+  })
+
+  p.resolve('ok')
+  p.reject('ok')
+})
